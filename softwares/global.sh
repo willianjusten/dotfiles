8db18c42bc3ade1220db6338e32e790d3283c563
Willian Justen <willianjustenqui@gmail.com>
2015-03-09 18:22:15 -0300

    Add new softwares

---
+++ softwares/global.sh
@@ -0,0 +1,29 @@
+# cask
+brew install caskroom/cask/brew-cask
+
+# add formulae to homebrew
+brew tap homebrew/science # science
+brew tap caskroom/fonts # fonts
+brew tap caskroom/versions
+
+# langs
+brew install python # python
+
+# remove outdated versions from the cellar
+brew cleanup
+
+# browsers
+brew cask install firefox 2> /dev/null
+brew cask install google-chrome 2> /dev/null
+
+# dev apps
+brew cask install imageoptim 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install virtualbox 2> /dev/null
+brew cask install vagrant 2> /dev/null
+
+# apps
+brew cask install vlc 2> /dev/null
+brew cask install transmission 2> /dev/null
+brew cask install rdio 2> /dev/null
+brew cask install spotify 2> /dev/null
