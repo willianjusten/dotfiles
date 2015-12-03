f794f14b68bdfdad200d7c28b73d8a40bad2cafb
Willian Justen <willianjustenqui@gmail.com>
2015-12-03 02:10:46 -0200

    Update global.sh

--- node/global.sh
+++ node/global.sh
@@ -4,8 +4,9 @@ source common/functions.sh
 if which node &> /dev/null; then
     msg_checking "node"
 else
-    msg_install "node" "git clone git://github.com/ry/node.git"
-    brew install node
+    msg_install "node" "npm install -g n"
+    npm install -g n
+    sudo n stable
     echo "OK"
 fi
 
@@ -45,15 +46,6 @@ else
     echo "OK"
 fi
 
-# jade
-if which jade &> /dev/null; then
-    msg_checking "jade"
-else
-    msg_install "jade" "npm install -g jade"
-    npm install -g jade
-    echo "OK"
-fi
-
 # browserify
 if which browserify &> /dev/null; then
     msg_checking "browserify"
@@ -61,42 +53,6 @@ else
     msg_install "browserify" "npm install -g browserify"
     npm install -g browserify
     echo "OK"
-fi
-
-# vtop
-if which vtop &> /dev/null; then
-    msg_checking "vtop"
-else
-    msg_install "vtop" "npm install vtop -g"
-    npm install vtop -g
-    echo "OK"
-fi
-
-# jshint
-if which jshint &> /dev/null; then
-    msg_checking "jshint"
-else
-    msg_install "jshint" "npm install jshint -g"
-    npm install jshint -g
-    echo "OK"
-fi
-
-# csslint
-if which csslint &> /dev/null; then
-    msg_checking "csslint"
-else
-    msg_install "csslint" "npm install -g csslint"
-    npm install -g csslint
-    msg_ok "OK"
-fi
-
-# TMI (Too Many Images) - discover your image weight on the web [alpha]
-if which tmi &> /dev/null; then
-    msg_checking "tmi"
-else
-    msg_install "tmi" "npm install -g tmi"
-    npm install -g tmi
-    msg_ok "OK"
 fi
 
 # bower
