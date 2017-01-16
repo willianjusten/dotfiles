76a3440ad99aacecd82b59485f525ddcd6c4958a
Willian Justen <willianjustenqui@gmail.com>
2017-01-16 21:25:27 +0000

    Add Franz App

--- softwares/global.sh
+++ softwares/global.sh
@@ -26,4 +26,5 @@ brew cask install vlc 2> /dev/null
 brew cask install transmission 2> /dev/null
 brew cask install spotify 2> /dev/null
 brew cask install slack 2> /dev/null
+brew cask install franz 2> /dev/null
 brew cask install coconutbattery 2> /dev/null
