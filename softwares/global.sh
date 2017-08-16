8d701a612d7f3821a139e20dfab038b6f57feeed
Willian Justen <willianjustenqui@gmail.com>
2017-08-16 19:53:06 -0300

    Update softwares

--- softwares/global.sh
+++ softwares/global.sh
@@ -19,15 +19,17 @@ brew cask install google-chrome 2> /dev/null
 # dev apps
 brew cask install imageoptim 2> /dev/null
 brew cask install iterm2 2> /dev/null
-brew cask install sublime-text3 2> /dev/null
+brew cask install visual-studio-code 2> /dev/null
+brew cask install psequel 2> /dev/null
 
 # apps
 brew cask install vlc 2> /dev/null
 brew cask install transmission 2> /dev/null
 brew cask install spotify 2> /dev/null
-brew cask install slack 2> /dev/null
 brew cask install franz 2> /dev/null
+brew cask install skype 2> /dev/null
 brew cask install coconutbattery 2> /dev/null
 brew cask install screenhero 2> /dev/null
 brew cask install zoomus 2> /dev/null
 brew cask install stremio 2> /dev/null
+brew cask install handbrake 2> /dev/null
